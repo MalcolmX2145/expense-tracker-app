d0c56638a3f74bf09da0a4dfe07aef68c86658e6
MalcolmX2145 <ekajjamalcolm@gmail.com>
2025-02-02 12:26:41 +0300

    fixed env

--- frontend/src/supabase-client.js
+++ frontend/src/supabase-client.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://izolyxqxrsxwsslrjiae.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6b2x5eHF4cnN4d3NzbHJqaWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0ODA5MTEsImV4cCI6MjA1NDA1NjkxMX0.O08Q4E1xHgjVxRwe07bfc6ubVzszrPeYRXWgdRaRVlg";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
